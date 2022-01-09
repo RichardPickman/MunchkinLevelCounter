434ec8cfd1c288253f3d34378bd21f76e11ca2ed
RichardPickman <drezzerock@gmail.com>
2022-01-10 00:38:35 +0300

    feat: implement new player handler to server | implement handler of playerid

--- server/index.mjs
+++ server/index.mjs
@@ -7,44 +7,57 @@ import { getWebSocketServer } from './ws/index.mjs'
 import * as wsCache from './ws/cache.mjs';
 
 
-
 const app = async () => {
     const config = getDbConfig();
     const wss = getWebSocketServer()
     const client = await createConnection(config);
 
     wss.on('connection', ws => {
-
-        console.log('Connected')
         
         const playerId = getPlayerId()
         wsCache.set(playerId, ws)
-
         ws.on('message', async arrayBuffer => {
+            console.log('LOG: Connection established')
             const data = arrayBufferToJSON(arrayBuffer);
             console.log(data);
     
             if (!data) {
                 return console.log("Wrong data: ", arrayBuffer.toString());
             };
-            
-            let result = await handleAction(data, client);
-            const resultString = JSON.stringify(result);
 
-            result.players.forEach((player) => {                
-                const wsPlayer = wsCache.get(player.playerId);
+            // HANDLE NEW PLAYER AND JSON RESPONSE 
+            if (data.storageId){
+                const playerId = getPlayerId()
 
+                wsCache.set(playerId, ws)
+
+                let wsPlayer = wsCache.get(playerId)
+                let resultString = JSON.stringify({ playerId })
+
+                console.log(resultString)
+                
                 if (wsPlayer) {
                     wsPlayer.send(resultString);
                 }
-            });
+            } else {
+                let result = await handleAction(data, client);
+                let resultString = JSON.stringify(result);
+
+                result.players.forEach((player) => {                
+                    const wsPlayer = wsCache.get(player.playerId);
+
+                    if (wsPlayer) {
+                        wsPlayer.send(resultString);
+                    }
+                });
+            }
             
         });
     
-        ws.send(JSON.stringify({ playerId }));
+        // ws.send(JSON.stringify({ playerId }));
     });
     
-    console.log('started')
+    console.log('LOG: Server has been started')
 }
 
 app()
--- ui/pages/_app.tsx
+++ ui/pages/_app.tsx
@@ -1,11 +1,12 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { useEffect, useRef, useState } from 'react';
+import { handleStorage } from '../components/storage'
 
 function MyApp({ Component, pageProps }: AppProps) {
     const ws = useRef<WebSocket|null>(null);
     const [connected, setConnected] = useState(false)
-    const [playerId, setMe] = useState<{ playerId: string | null }>(null);
+    const [playerId, setPlayerId] = useState<{ playerId: string | null }>(null);
     const [players, setPlayers] = useState<{ [k: string]: any }[]>([]);
     const [sessionId, setSessionId] = useState()
 
@@ -14,19 +15,33 @@ function MyApp({ Component, pageProps }: AppProps) {
 
         ws.current.onmessage = (message) => {
             const mess = JSON.parse(message.data)
-            console.log(mess)
+            const payload = {
+                id: mess.playerId,
+                action: 'add'
+            }
     
             if (mess.playerId) {
-                setMe(mess.playerId)
+                const setPlayer = handleStorage(payload)
+                setPlayerId(setPlayer)
+                console.log('YOUR PERMANENT ID IS: ' + playerId)
             } else if (mess.sessionId) {
                 setSessionId(mess.sessionId)
                 setPlayers(mess.players)
             }
         };
         
-
-        ws.current.onopen = (event) => {
+        ws.current.onopen = () => {
             setConnected(true)
+            console.log('LOG: CONNECTED TO SERVER')
+            const storageAction = handleStorage({action: 'get'})
+
+            if (storageAction === null) {
+                console.log('LOG: NO STORAGE DATA... REQUESTING NEW ID')
+                ws.current.send(JSON.stringify({ storageId: 'getId' }))
+            } else {
+                setPlayerId(storageAction)
+                console.log('LOG: PLAYER ID FOUND. YOUR ID: ' + playerId)
+            }
         }
     
         return () => ws?.current.close();
