59f303f3372fa5872d705ef121fdf1758c23e7f9
Richard Pickman <drezzerock@gmail.com>
2022-01-19 16:05:20 +0100

    fix: add a field to player data

--- server/actions/helpers.mjs
+++ server/actions/helpers.mjs
@@ -7,5 +7,6 @@ export const createPlayer = (playerId, isOwner = false) => ({
         color: getColor(),
         level: 1,
         equipment: 0,
-        temporaryBonus: 0
+        temporaryBonus: 0,
+        isInside: true
 });
