c518b7ab7212f64e6df136f2a698c9cceee6e9b8
RichardPickman <drezzerock@gmail.com>
2022-04-03 03:57:27 +0200

    fix: few fixes arrived

--- components/mainpage/index.tsx
+++ components/mainpage/index.tsx
@@ -2,17 +2,15 @@ import { useRef } from 'react';
 import styles from './Mainpage.module.css'
 
 
-export const MainPage = ({ create, join }: {[k:string]: any}) => {
+export const MainPage = ({ create, join }: { [k: string]: any }) => {
     const inputRef = useRef<HTMLInputElement>();
 
     return (
-        <div className={styles.homePage}>
-            <div className={styles.btngroup}>
-                <button onClick={create}>Create game</button>
-                <div className={styles.joining}>
-                    <input ref={inputRef} placeholder='Enter id'></input>
-                    <button onClick={() => join(inputRef.current.value)}>Join game</button>
-                </div>
+        <div className={styles.root}>
+            <button className={styles.button} onClick={create}>Create game</button>
+            <div className={styles.joining}>
+                <input className={styles.input} ref={inputRef} placeholder='Enter id'></input>
+                <button className={styles.button} onClick={() => join(inputRef.current.value)}>Join game</button>
             </div>
         </div>
     );
--- components/session/index.tsx
+++ components/session/index.tsx
@@ -2,16 +2,18 @@ import { Player } from '../Player'
 import * as Types from '../../types'
 
 import styles from './Session.module.css';
-
 interface Props extends Types.Session {
     playerId: Types.PlayerId,
     update: (args: Partial<Types.Player>) => void
 }
 
 export const Session = ({ playerId, players, update }: Props) => {
+    const index = players.findIndex(player => player.playerId === playerId)
+    const sortedPlayers = [...players.slice(index), ...players.slice(0, index)]
+
     return (
         <div className={styles.root}>
-            {players.map(player => player.isActive && (
+            {sortedPlayers.map(player => player.isActive && (
                 <Player
                     {...player}
                     onClick={playerId === player.playerId && update}
