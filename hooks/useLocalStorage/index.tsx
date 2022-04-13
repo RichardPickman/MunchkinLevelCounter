50902d1b6291ea561c511b3feca7a6069f364f1d
RichardPickman <drezzerock@gmail.com>
2022-04-13 21:44:01 +0200

    feat: add setter to hook

--- hooks/useLocalStorage/index.tsx
+++ hooks/useLocalStorage/index.tsx
@@ -1,11 +1,17 @@
 import { useState } from "react";
 
-export const useLocalStorage = (key) => {
-    const [value] = useState(() => {
-        // почитай про globalThis, это новая фича языка
-        return globalThis.localStorage && globalThis.localStorage.getItem(key)
-    });
+export const useLocalStorage = (type: string, key: string, value?: string) => {
+    switch (type) {
+        case 'get': {
+            const [value] = useState(() => {
+                // почитай про globalThis, это новая фича языка
+                return globalThis.localStorage && globalThis.localStorage.getItem(key)
+            });
 
-    return value;
+            return value
+        }
+        case 'set':
+            globalThis.localStorage && globalThis.localStorage.setItem(key, value)
+    }
 }
 
