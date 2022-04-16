155eda015228efb899a8cf00432604ee212a00f8
RichardPickman <drezzerock@gmail.com>
2022-04-16 19:35:55 +0300

    feat: nickname handler

--- components/App/index.tsx
+++ components/App/index.tsx
@@ -1,37 +1,61 @@
+import { useCallback, useEffect } from 'react';
 import { useAppStore } from '../../hooks/useAppStore';
 import { useWebSocket } from '../../hooks/useWebSocket';
 import { Header } from '../Header';
-import { MainPage } from '../Mainpage';
-import { Clipboard } from '../Clipboard';
 import { Session } from '../Session';
-
+import { Clipboard } from '../Clipboard';
+import { Button } from '../Button';
+import { Input } from '../Input';
 import * as Types from '../../types';
 import styles from './App.module.css';
 
-
 export const App = () => {
-    const [{ sessionId, playerId, players }, dispatch] = useAppStore()
-    const ws = useWebSocket(message => dispatch(message))
+    const [{ sessionId, playerId, players, nickname }, dispatch] = useAppStore();
+    const ws = useWebSocket(message => dispatch(message));
 
-    const dispatchRemoteAction = (type, payload) => ws.send({ type, payload })
+    const dispatchRemoteAction = (type, payload) => ws.send({ type, payload });
 
-    const create = (nickname: Partial<Types.Player>) => dispatchRemoteAction('session/create', { playerId, nickname });
-    const join = (sessionId: Types.SessionId, nickname: Partial<Types.Player>) => dispatchRemoteAction('session/join', { sessionId, playerId, nickname });
-    const update = (payload: Partial<Types.Player>) => dispatchRemoteAction('session/update', { sessionId, playerId, ...payload });
+    const setNickname = useCallback((nickname: Types.Nickname) => dispatch({ type: 'nickname/set', payload: { nickname } }), [dispatch]);
+    const setSessionId = useCallback((sessionId: Types.SessionId) => dispatch({ type: 'sessionId/set', payload: { sessionId } }), [dispatch]);
+
+    useEffect(() => {
+        const storedNickname = localStorage.getItem('nickname');
+
+        if (storedNickname) {
+            setNickname(storedNickname);
+        }
+    }, [setNickname]);
+
+    useEffect(() => localStorage.setItem('nickname', nickname), [nickname]);
+
+    const create = () => dispatchRemoteAction('session/create', { playerId, nickname });
+    const join = () => dispatchRemoteAction('session/join', { sessionId, playerId, nickname });
     const exit = () => dispatchRemoteAction('session/exit', { sessionId, playerId });
+    const update = (payload: Partial<Types.Player>) => dispatchRemoteAction('session/update', { sessionId, playerId, ...payload });
 
-    if (!sessionId) {
-        return <MainPage create={create} join={join} />
+    const isSessionActive = sessionId && playerId && players.length;
+
+    if (isSessionActive) {
+        return (
+            <div className={styles.root}>
+                <Header>
+                    <Clipboard value={sessionId} />
+                    <Button onClick={exit} text="Exit" />
+                    <Button onClick={() => update({ temporaryBonus: 0 })} text="Next turn" />
+                </Header>
+                <Session players={players} playerId={playerId} onUpdate={update} />
+            </div>
+        );
     }
 
     return (
         <div className={styles.root}>
-            <Header>
-                <Clipboard value={sessionId} />
-                <button onClick={exit}>Exit</button>
-                <button onClick={() => update({ temporaryBonus: 0 })}>Next turn</button>
-            </Header>
-            <Session players={players} playerId={playerId} onUpdate={update} />
+            <Input onChange={setNickname} placeholder="Your name" value={nickname} />
+            <Button onClick={create} text="Create" />
+            <div>
+                <Input onChange={setSessionId} placeholder="Session Id" />
+                <Button onClick={join} text="Join" />
+            </div>
         </div>
-    )
+    );
 }
